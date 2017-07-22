5b635f627cb69269e76228432c4a54636a0af877
Sho Mizutani <lowply@github.com>
2017-07-22 20:54:31 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -av --delete -e "ssh -l ${SSH_USER} -p ${SSH_PORT}" bin templates public ${HOST}:${TARGET}/
-ssh -p ${SSH_PORT} -l ${SSH_USER} ${HOST} "supervisorctl restart ${DOMAIN}"
+rsync -i ./deploy_rsa -av --delete -e "ssh -l ${SSH_USER} -p ${SSH_PORT}" bin templates public ${HOST}:${TARGET}/
+ssh -i ./deploy_rsa -p ${SSH_PORT} -l ${SSH_USER} ${HOST} "supervisorctl restart ${DOMAIN}"
