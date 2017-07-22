6173b1fac43330a31ed557cce7bdd40dc91dac62
Sho Mizutani <lowply@github.com>
2017-07-22 21:15:16 +0900

    Use gpg to encrypt env file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-rsync -i ./deploy_rsa -av --delete -e "ssh -l ${SSH_USER} -p ${SSH_PORT}" bin templates public ${HOST}:${TARGET}/
-ssh -i ./deploy_rsa -p ${SSH_PORT} -l ${SSH_USER} ${HOST} "supervisorctl restart ${DOMAIN}"
+gpg -d env.gpg > env
+. ./env
+rsync -av --delete -e "ssh -l ${SSH_USER} -p ${SSH_PORT}" bin templates public ${HOST}:${TARGET}/
+ssh -p ${SSH_PORT} -l ${SSH_USER} ${HOST} "${CMD}"
+rm ./env
