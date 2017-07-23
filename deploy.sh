cdc782e8250fd2eb7af05733f4ede7ab390e18b3
Sho Mizutani <lowply@github.com>
2017-07-23 19:11:30 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-gpg -d env.gpg > env
+gpg -qd env.gpg > env
+
 . ./env
+
+for V in "${SSH_USER}" "${SSH_PORT}" "${HOST}" "${TARGET}" "${CMD}"; do
+	[ -z "${V}" ] && { echo "One or more variable is empty"; exit 1; }
+done
+
 rsync -av --delete -e "ssh -l ${SSH_USER} -p ${SSH_PORT}" bin templates public ${HOST}:${TARGET}/
 ssh -p ${SSH_PORT} -l ${SSH_USER} ${HOST} "${CMD}"
+
 rm ./env
