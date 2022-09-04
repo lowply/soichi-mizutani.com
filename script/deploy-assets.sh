6923b93ff51287026fc9cb380d5240c79bbff62d
Sho Mizutani <lowply@github.com>
2022-09-05 00:24:43 +0900

    Add account name

--- script/deploy-assets.sh
+++ script/deploy-assets.sh
@@ -3,6 +3,7 @@
 find . -name .DS_Store -delete -exec echo removed: {} \;
 
 az storage blob sync \
+    --account-name soichi \
     --source ./static/assets \
     --container '$web/assets' \
     --delete-destination true
--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,7 @@
 find . -name .DS_Store -delete -exec echo removed: {} \;
 
 az storage blob sync \
+    --account-name soichi \
     --source ./public \
     --container '$web' \
     --exclude-path assets \
